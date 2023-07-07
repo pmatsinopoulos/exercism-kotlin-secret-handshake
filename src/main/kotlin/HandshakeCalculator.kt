2179577cf0f7a9d8658442d934c9f12d37052a1e
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-07 12:26:52 +0300

    Use a forEach to iterate the bitpositions

--- src/main/kotlin/HandshakeCalculator.kt
+++ src/main/kotlin/HandshakeCalculator.kt
@@ -1,21 +1,26 @@
+private val bitPositionMapToSignals = mapOf(
+    1 to Signal.WINK,
+    2 to Signal.DOUBLE_BLINK,
+    4 to Signal.CLOSE_YOUR_EYES,
+    8 to Signal.JUMP
+)
+
 object HandshakeCalculator {
     fun calculateHandshake(number: Int): List<Signal> {
         val actions: MutableList<Signal> = mutableListOf()
-        if (number and 1 == 1) {
-            actions.add(Signal.WINK)
+
+        listOf(1, 2, 4, 8).forEach { bitPosition ->
+            bitPositionMapToSignals[bitPosition]?.let { signal ->
+                if (number and bitPosition == bitPosition) actions.add(
+                    signal
+                )
+            }
         }
-        if (number and 2 == 2) {
-            actions.add(Signal.DOUBLE_BLINK)
-        }
-        if (number and 4 == 4) {
-            actions.add(Signal.CLOSE_YOUR_EYES)
-        }
-        if (number and 8 == 8) {
-            actions.add(Signal.JUMP)
-        }
+
         if (number and 16 == 16) {
             actions.reverse()
         }
+
         return actions.toList()
     }
 }
