7e1b2618e3cb3083d07653ff077b037e6c47b2a6
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-07 12:18:17 +0300

    First iteration makes all tests pass

--- src/main/kotlin/HandshakeCalculator.kt
+++ src/main/kotlin/HandshakeCalculator.kt
@@ -1,5 +1,21 @@
 object HandshakeCalculator {
     fun calculateHandshake(number: Int): List<Signal> {
-        TODO("Implement the function to complete the task")
+        val actions: MutableList<Signal> = mutableListOf()
+        if (number and 1 == 1) {
+            actions.add(Signal.WINK)
+        }
+        if (number and 2 == 2) {
+            actions.add(Signal.DOUBLE_BLINK)
+        }
+        if (number and 4 == 4) {
+            actions.add(Signal.CLOSE_YOUR_EYES)
+        }
+        if (number and 8 == 8) {
+            actions.add(Signal.JUMP)
+        }
+        if (number and 16 == 16) {
+            actions.reverse()
+        }
+        return actions.toList()
     }
 }
--- src/test/kotlin/HandshakeCalculatorTest.kt
+++ src/test/kotlin/HandshakeCalculatorTest.kt
@@ -8,95 +8,96 @@ class HandshakeCalculatorTest {
     fun testThatInput1YieldsAWink() {
         assertEquals(
             listOf(Signal.WINK),
-            HandshakeCalculator.calculateHandshake(1))
+            HandshakeCalculator.calculateHandshake(1)
+        )
     }
 
-    @Ignore
     @Test
     fun testThatInput2YieldsADoubleBlink() {
         assertEquals(
             listOf(Signal.DOUBLE_BLINK),
-            HandshakeCalculator.calculateHandshake(2))
+            HandshakeCalculator.calculateHandshake(2)
+        )
     }
 
-    @Ignore
     @Test
     fun testThatInput4YieldsACloseYourEyes() {
         assertEquals(
             listOf(Signal.CLOSE_YOUR_EYES),
-            HandshakeCalculator.calculateHandshake(4))
+            HandshakeCalculator.calculateHandshake(4)
+        )
     }
 
-    @Ignore
     @Test
     fun testThatInput8YieldsAJump() {
         assertEquals(
             listOf(Signal.JUMP),
-            HandshakeCalculator.calculateHandshake(8))
+            HandshakeCalculator.calculateHandshake(8)
+        )
     }
 
-    @Ignore
     @Test
     fun testAnInputThatYieldsTwoActions() {
         assertEquals(
             listOf(Signal.WINK, Signal.DOUBLE_BLINK),
-            HandshakeCalculator.calculateHandshake(3))
+            HandshakeCalculator.calculateHandshake(3)
+        )
     }
 
-    @Ignore
     @Test
     fun testAnInputThatYieldsTwoReversedActions() {
         assertEquals(
             listOf(Signal.DOUBLE_BLINK, Signal.WINK),
-            HandshakeCalculator.calculateHandshake(19))
+            HandshakeCalculator.calculateHandshake(19)
+        )
     }
 
-    @Ignore
     @Test
     fun testReversingASingleActionYieldsTheSameAction() {
         assertEquals(
             listOf(Signal.JUMP),
-            HandshakeCalculator.calculateHandshake(24))
+            HandshakeCalculator.calculateHandshake(24)
+        )
     }
 
-    @Ignore
     @Test
     fun testReversingNoActionsYieldsNoActions() {
         assertEquals(
             emptyList(),
-            HandshakeCalculator.calculateHandshake(16))
+            HandshakeCalculator.calculateHandshake(16)
+        )
     }
 
-    @Ignore
     @Test
     fun testInputThatYieldsAllActions() {
         assertEquals(
             listOf(Signal.WINK, Signal.DOUBLE_BLINK, Signal.CLOSE_YOUR_EYES, Signal.JUMP),
-            HandshakeCalculator.calculateHandshake(15))
+            HandshakeCalculator.calculateHandshake(15)
+        )
     }
 
-    @Ignore
     @Test
     fun testInputThatYieldsAllActionsReversed() {
         assertEquals(
             listOf(Signal.JUMP, Signal.CLOSE_YOUR_EYES, Signal.DOUBLE_BLINK, Signal.WINK),
-            HandshakeCalculator.calculateHandshake(31))
+            HandshakeCalculator.calculateHandshake(31)
+        )
     }
 
-    @Ignore
     @Test
     fun testThatInput0YieldsNoActions() {
         assertEquals(
             emptyList(),
-            HandshakeCalculator.calculateHandshake(0))
+            HandshakeCalculator.calculateHandshake(0)
+        )
     }
 
-    @Ignore
     @Test
     fun testThatInputWithLower5BitsNotSetYieldsNoActions() {
         assertEquals(
             emptyList(),
-            HandshakeCalculator.calculateHandshake(32))
+            HandshakeCalculator.calculateHandshake(32)
+        )
     }
 
 }
