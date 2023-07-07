ae9f5292e382841bf987c59ff7fad91fb5c4633c
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-07 13:13:07 +0300

    Use the Signal to encapsulate the mutation logic for the actions

--- src/main/kotlin/HandshakeCalculator.kt
+++ src/main/kotlin/HandshakeCalculator.kt
@@ -1,20 +1,9 @@
-private val bitPositionMapToSignals = mapOf(
-    1 to Signal.WINK,
-    2 to Signal.DOUBLE_BLINK,
-    4 to Signal.CLOSE_YOUR_EYES,
-    8 to Signal.JUMP
-)
-
 object HandshakeCalculator {
     fun calculateHandshake(number: Int): List<Signal> {
         val actions: MutableList<Signal> = mutableListOf()
 
         Signal.values().forEach { signal ->
-            if (signal.shallWeAdd(number)) actions.add(signal)
-        }
-
-        if (number and 16 == 16) {
-            actions.reverse()
+            signal.action(number, actions)
         }
 
         return actions.toList()
--- src/main/kotlin/Signal.kt
+++ src/main/kotlin/Signal.kt
@@ -1,9 +1,20 @@
 enum class Signal(private val bitPosition: Int) {
-
     WINK(1),
     DOUBLE_BLINK(2),
     CLOSE_YOUR_EYES(4),
-    JUMP(8);
+    JUMP(8),
+    REVERSE(16) {
+        override fun handle(actions: MutableList<Signal>) {
+            actions.reverse()
+        }
+    };
 
-    fun shallWeAdd(number: Int) = number and bitPosition == bitPosition
+    private fun matches(number: Int) = number and bitPosition == bitPosition
+    open fun handle(actions: MutableList<Signal>) {
+        actions.add(this)
+    }
+
+    fun action(number: Int, actions: MutableList<Signal>) {
+        if (matches(number)) handle(actions)
+    }
 }
